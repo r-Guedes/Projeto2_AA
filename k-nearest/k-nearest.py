033d7e75c3fca2d8d98def88f48eb47d7d341cd9
Daniela Simões <danielasimoes@ua.pt>
2018-12-29 21:53:25 +0000

    learning curve.

--- k-nearest/k-nearest.py
+++ k-nearest/k-nearest.py
@@ -1,13 +1,13 @@
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, mean_squared_error
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.model_selection import learning_curve
+import numpy as np
 
 
 import sys
@@ -22,6 +22,9 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
     scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+
     scaler.fit(x_train)
     x_train = scaler.transform(x_train)
     x_test = scaler.transform(x_test)
@@ -38,9 +41,43 @@ if __name__ == '__main__':
     print("Accuracy Test:", metrics.accuracy_score(y_test, y_pred))
     print("Accuracy Train:", metrics.accuracy_score(y_train, y_pred_train))
 
+    print("Mean squared error - test set:", mean_squared_error(y_test, y_pred))
+    print("Mean squared error - training set:", mean_squared_error(y_train, y_pred_train))
+    print("Cross_validation_evaluate: ", cross_val_score(clf,x_train,y_train,cv=10))
+
+    # Create CV training and test scores for various training set sizes
+    train_sizes, train_scores, test_scores = learning_curve(clf,
+                                                            X,
+                                                            y,
+                                                            cv=10,
+                                                            n_jobs=-1,
+                                                            train_sizes=np.linspace(0.01, 1.0, 50))
+
+    # Create means and standard deviations of training set scores
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+
+    # Create means and standard deviations of test set scores
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    # Draw lines
+    plt.plot(train_sizes, train_mean, '--', color="#111111", label="Training score")
+    plt.plot(train_sizes, test_mean, color="#111111", label="Cross-validation score")
+
+    # Draw bands
+    plt.fill_between(train_sizes, train_mean - train_std, train_mean + train_std, color="#DDDDDD")
+    plt.fill_between(train_sizes, test_mean - test_std, test_mean + test_std, color="#DDDDDD")
+
+    # Create plot
+    plt.title("Learning Curve")
+    plt.xlabel("Training Set Size"), plt.ylabel("Score"), plt.legend(loc="best")
+    plt.tight_layout()
+    plt.show()
+
     """
     Finding the best K
-    """
+
 
     k_range = list(range(1, 31))
     weight_options = ['uniform', 'distance']
@@ -56,7 +93,7 @@ if __name__ == '__main__':
     plt.xlabel('Value of K for KNN')
     plt.ylabel('Cross-Validated Accuracy')
     plt.show()
-
+    """
 
 
 
